2ad49641e2ab33d1bbf457aa96eb86474db7874d
Dumindu Udara <98957798+DumindUdara@users.noreply.github.com>
2025-02-08 11:50:09 +0530

    prepare callbacks addedd

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -24,11 +24,11 @@ class PrepareBaseModelConfig:
 
 
 
-# @dataclass(frozen=True)
-# class PrepareCallbacksConfig:
-#     root_dir: Path
-#     tensorboard_root_log_dir: Path
-#     checkpoint_model_filepath: Path
+@dataclass(frozen=True)
+class PrepareCallbacksConfig:
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: Path
 
 
 
