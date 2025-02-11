58a6713487e927458eed37381be214d60d714243
Dumindu Udara <98957798+DumindUdara@users.noreply.github.com>
2025-02-11 14:41:04 +0530

    traing added

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraining
 
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
-# from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
-# from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+
+#from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -35,17 +36,36 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Training"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_trainer = ModelTrainingPipeline()
-#    model_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
 
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+try:
+    config = ConfigurationManager()
+    prepare_callbacks_config = config.get_prepare_callback_config()
+    prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
+    callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+
+    training_config = config.get_training_config()
+    training = Training(config=training_config)
+    training.get_base_model()
+    training.train_valid_generator()
+    training.train(
+        callback_list=callback_list
+    )
+    
+except Exception as e:
+    raise e
 
 
 
